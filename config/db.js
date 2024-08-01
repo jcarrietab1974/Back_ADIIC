5b69ea699105f6129476647ed99a28fd268da28d
juan arrieta <caporo1974@yahoo.es>
2024-07-31 21:55:32 -0500

    Conenctando la aplicación a la base de datos en MongoDB

--- config/db.js
+++ config/db.js
@@ -4,8 +4,8 @@ const conectarDB = async () => {
 
     try{
        const connection = await mongoose.connect(
-        "mongodb+srv://RichardPardo:9737854@cluster0.w5ubywx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-        // "mongodb+srv://jcarrietab:Jc7rie4tab@cluster0.uxoyy2r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+        //"mongodb+srv://RichardPardo:9737854@cluster0.w5ubywx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+        "mongodb+srv://jcarrietab:Jc7rie4tab@cluster0.uxoyy2r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
         {
             UseNewUrlParser: true,
             useUnifiedTopology: true,
