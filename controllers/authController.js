9c7c29c44767ef51029c4b7bb48ac88e3d951d3b
juan arrieta <caporo1974@yahoo.es>
2025-03-01 11:44:41 -0500

    En Usuarios.js se añadió el campo 'rol' al esquema de Usuarios para definir el rol de cada usuario en el sistema. Además, se mantiene la estructura y validaciones existentes en el modelo. En authController.js se modificó `exports.usuarioAutenticado` para que la respuesta ahora incluya el rol del usuario. Además, se agregó el rol al payload del token generado en `autenticarUsuario`, permitiendo gestionar permisos según el rol asignado.

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,45 +6,53 @@ require("dotenv").config({ path: "variables.env" });
 exports.autenticarUsuario = async (req, res) => {
   const { password, email } = req.body;
   try {
-    //Revisar que sea un usuario registrado
+    // Revisar que sea un usuario registrado
     let usuario = await Usuario.findOne({ email });
 
     if (!usuario) {
       return res.status(404).json({ msg: "El usuario no existe" });
     }
-    //Revisar el pasword
+
+    // Revisar el password
     const passwordCorrecto = await bcryptjs.compare(password, usuario.password);
     if (!passwordCorrecto) {
       return res.status(400).json({ msg: "Password incorrecto" });
     }
 
-    //Si todo es correcto : crear y firmar un token
+    // Si todo es correcto: crear y firmar un token
     const payload = {
-      usuario: { id: usuario.id },
+      usuario: { id: usuario.id, rol: usuario.rol }, // Agregar rol al payload
     };
     //res.json(payload);
     jwt.sign(
       payload,
       process.env.SECRETA,
       {
-        expiresIn: "30d", //30 dias
+        expiresIn: "30d", // 30 días
       },
       (error, token) => {
         if (error) throw error;
-        //Mensaje de confirmación
+        // Mensaje de confirmación
         res.json({ token });
       }
     );
   } catch (error) {
     console.log(error);
+    res.status(500).json({ msg: "Hubo un error en el servidor" });
   }
 };
+
 // Cambio
 exports.usuarioAutenticado = async (req, res) => {
   try {
     const usuario = await Usuario.findById(req.usuario.id);
-    res.json({ usuario });
+    if (!usuario) {
+      return res.status(404).json({ msg: "Usuario no encontrado" });
+    }
+
+    res.json({ usuario, rol: usuario.rol });
   } catch (error) {
-    res.status(500).json({ msg: "Hubo un error" });
+    console.error(error);
+    res.status(500).json({ msg: "Hubo un error en el servidor" });
   }
 };
--- models/Usuarios.js
+++ models/Usuarios.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const UsuariosSchema = mongoose.Schema({
   nombre: { type: String, required: true, trim: true },
   email: { type: String, requiere: true, trim: true, unique: true },
+  rol: { type: String, required: true, trim: true },
   password: { type: String, requiere: true, trim: true },
   registro: { type: Date, default: Date.now() },
 });
